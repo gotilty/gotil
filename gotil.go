707760d0300944dfd4647e4220fc53adf8228cc1
unknown <ertgrlkutluer@gmail.com>
2022-03-28 01:46:17 +0300

    feat: to string methods implemented

--- gotil.go
+++ gotil.go
@@ -3,6 +3,7 @@ package gotil
 import (
 	"fmt"
 	"reflect"
+	"strconv"
 )
 
 // If source is string. Fastest and most reliable way to check if string is empty is s != ""
@@ -39,17 +40,15 @@ func IsAssigned(a interface{}) bool {
 		return isAssignedUint(val.Uint())
 	case reflect.Bool:
 		return isAssignedBool(val.Bool())
+	case reflect.Pointer, reflect.Chan, reflect.Func, reflect.Struct, reflect.UnsafePointer:
+		tt := reflect.Zero(val.Type())
+		return !reflect.DeepEqual(val, tt)
+	case reflect.Array, reflect.Slice:
+		return val.Len() > 0
 	default:
-		kind := val.Kind()
-		switch kind {
-		case reflect.Pointer, reflect.Chan, reflect.Func, reflect.Struct, reflect.UnsafePointer:
-			tt := reflect.Zero(val.Type())
-			return !reflect.DeepEqual(val, tt)
-		case reflect.Array, reflect.Slice:
-			return val.Len() > 0
-		}
 		return false
 	}
+
 }
 
 func isNotAssignedStr[K string](s K) bool {
@@ -85,15 +84,67 @@ func IsNotAssigned(a interface{}) bool {
 		return isNotAssignedUint(val.Uint())
 	case reflect.Bool:
 		return isNotAssignedBool(val.Bool())
+	case reflect.Pointer, reflect.Chan, reflect.Func, reflect.Struct, reflect.UnsafePointer:
+		tt := reflect.Zero(val.Type())
+		return reflect.DeepEqual(val, tt)
+	case reflect.Array, reflect.Slice:
+		return val.Len() == 0
 	default:
-		kind := val.Kind()
-		switch kind {
-		case reflect.Pointer, reflect.Chan, reflect.Func, reflect.Struct, reflect.UnsafePointer:
-			tt := reflect.Zero(val.Type())
-			return reflect.DeepEqual(val, tt)
-		case reflect.Array, reflect.Slice:
-			return val.Len() == 0
-		}
 		return false
+	}
+
+}
+
+func strToStr[K string](s K) string {
+	return fmt.Sprintf("%s", s)
+}
+func intToStr[K int | int32 | int64](s K) string {
+	return strconv.Itoa(int(s))
+}
+func floatToStr[K float32 | float64](s K) string {
+	return fmt.Sprintf("%f", s)
+}
+func uintToStr[K uint | uint8 | uint16 | uint32 | uint64](s K) string {
+	return strconv.FormatUint(uint64(s), 10)
+}
+func boolToStr[K bool](s K) string {
+	if s == true {
+		return "true"
+	} else {
+		return "false"
+	}
+}
+
+// ToString
+func ToString(a interface{}) string {
+	val := reflect.ValueOf(a)
+	switch val.Kind() {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return intToStr(val.Int())
+	case reflect.Float32, reflect.Float64:
+		return floatToStr(val.Float())
+	case reflect.String:
+		return strToStr(val.String())
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return uintToStr(val.Uint())
+	case reflect.Bool:
+		return boolToStr(val.Bool())
+	case reflect.Pointer, reflect.Chan, reflect.Func, reflect.Struct, reflect.UnsafePointer:
+		return val.String()
+	case reflect.Array, reflect.Slice:
+		var ret string = ""
+		var _for int = val.Len()
+		for i := 0; i < _for; i++ {
+			val := val.Index(i).Interface()
+			if i == _for-1 {
+				ret += fmt.Sprintf("%s", val)
+			} else {
+				ret += fmt.Sprintf("%s", val) + ", "
+			}
+
+		}
+		return ret
+	default:
+		return "aa"
 	}
 }
