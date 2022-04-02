39510e1a17161fc721f4cf4bdc20c8131ffc3b91
unknown <ertgrlkutluer@gmail.com>
2022-04-02 13:45:59 +0300

    feat: mapping with func

--- array/map.go
+++ array/map.go
@@ -3,16 +3,25 @@ package array
 import (
 	"fmt"
 	"reflect"
+
+	"github.com/gotilty/gotil/converter"
 )
 
-func Map(a interface{}, f func() interface{}) {
-	fmt.Println(f())
+func Map(a interface{}, f func(i interface{}) interface{}) interface{} {
 	val := reflect.ValueOf(a)
 	switch val.Kind() {
 	case reflect.Slice, reflect.Array:
-		for i := 0; i < val.Len(); i++ {
-			fmt.Println(val.Index(i))
+		typ := reflect.TypeOf(a).Elem().Kind()
+		if typ == reflect.Int || typ == reflect.Int8 || typ == reflect.Int16 || typ == reflect.Int32 || typ == reflect.Int64 {
+			for i := 0; i < val.Len(); i++ {
+				var res = []int64{}
+				vall := val.Index(i).Interface()
+				int_value, _ := converter.ToInt64(vall.(int))
+				fmt.Println(f(int_value))
+				// ress, _ := converter.ToInt64(f(int_value).(int))
+				a = append(res, int_value)
+			}
 		}
-
 	}
+	return a
 }
--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gotilty/gotil/array"
+	"github.com/gotilty/gotil/converter"
 )
 
 func main() {
-	g := greetings
-	array.Map("ertu", g)
+	s := square
+	sample_array1 := []int{9, -13, 4, -2, 3, 1, -10, 21, 12}
+	fmt.Println(array.Map(sample_array1, s))
 }
 
-func greetings() interface{} {
-	return ("ertu")
+func square(a interface{}) interface{} {
+	b, _ := converter.ToInt64(a)
+	return (b * 3)
 }
