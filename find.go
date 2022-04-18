497b5c1ab754a15c7c63c76a3e853a476271c346
hayrullahcansu <hayrullah.cansu@gmail.com>
2022-04-18 22:45:19 +0300

    feature: impl find & find_last

--- find.go
+++ find.go
@@ -1,7 +1,5 @@
 package gotil
 
-import "github.com/gotilty/gotil/internal"
-
 // FindBy iterates over elements of collection, returns an object of fist element predicate if returns true for.
 //	data := []int64{-100, -5, 30, 100}
 //	newData, _ := FindBy(data, func(val interface{}, i int) bool {
@@ -12,12 +10,106 @@ import "github.com/gotilty/gotil/internal"
 // 		}
 // 	})
 // 	// Output: 30
-func FindBy(a interface{}, f func(val interface{}, i int) bool) (interface{}, error) {
-	return internal.FindBy(a, f)
+func FindBy[T any](s []T, f func(val T) bool) T {
+	return FindByFromIndex(s, f, 0)
 }
 
 // FindByFromIndex iterates over elements of collection, returns an object of fist element predicate if returns true for.
 // It works same as FindBy just from parameter provides to set start index of given slice or array.
-func FindByFromIndex(a interface{}, f func(val interface{}, i int) bool, from int) (interface{}, error) {
-	return internal.FindByFromIndex(a, f, from)
+func FindByFromIndex[T any](s []T, f func(val T) bool, from int) T {
+	var result T
+	index := findIndex(s, f, 0)
+	if index > -1 {
+		return s[index]
+	}
+	return result
+}
+
+// FindLastBy iterates over elements of collection, returns an object of lastest element predicate if returns true for.
+// 	data := []int64{-100, -5, 30, 100}
+// 	newData, _ := FindLastBy(data, func(val interface{}, i int) bool {
+// 		if val.(int64) == 30 {
+// 			return true
+// 		} else {
+// 			return false
+// 		}
+// 	})
+// 	// Output: 30
+func FindLastBy[T any](s []T, f func(val T) bool) T {
+	return FindLastByFromIndex(s, f, 0)
+}
+
+// FindLastBy iterates over elements of collection, returns an object of lastest element predicate if returns true for.
+// It works same as FindLastBy just from parameter provides to set start index of given slice or array.
+func FindLastByFromIndex[T any](s []T, f func(val T) bool, from int) T {
+	var result T
+	index := findLastIndex(s, f, 0)
+	if index > -1 {
+		return s[index]
+	}
+	return result
+}
+
+func findIndex[T any](s []T, f func(val T) bool, from int) int {
+	length := len(s)
+	if length == 0 {
+		return -1
+	}
+	index := 0
+	if from < 0 {
+		index = max(length+index, 0)
+	} else {
+		index = min(index, length-1)
+	}
+	return baseFindIndex(s, f, index, false)
+}
+
+func findLastIndex[T any](s []T, f func(val T) bool, from int) int {
+	length := len(s)
+	if length == 0 {
+		return -1
+	}
+	index := length - 1
+	if from < 0 {
+		index = max(length+index, 0)
+	} else {
+		index = min(index, length-1)
+	}
+	return baseFindIndex(s, f, index, true)
+}
+
+func baseFindIndex[T any](s []T, f func(val T) bool, from int, right bool) int {
+	length := len(s)
+	index := from
+	if right {
+		for index < length {
+			value := s[index]
+			if f(value) {
+				return index
+			}
+			index--
+		}
+	} else {
+		for index < length {
+			value := s[index]
+			if f(value) {
+				return index
+			}
+			index++
+		}
+	}
+	return -1
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
---
+++ find_test.go
@@ -0,0 +1,163 @@
+package gotil_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/gotilty/gotil"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFindBy(t *testing.T) {
+	a := assert.New(t)
+	input := []int64{-100, -5, 30, 100}
+	expected := int64(30)
+	result := gotil.FindBy(input, func(val int64) bool {
+		return val == 30
+	})
+	a.Equal(expected, result)
+	input2 := []user{
+		{
+			name: "Micheal",
+			age:  27,
+		},
+		{
+			name: "Joe",
+			age:  30,
+		},
+		{
+			name: "Olivia",
+			age:  42,
+		},
+	}
+	result2 := gotil.FindBy(input2, func(val user) bool {
+		return val.name == "Olivia"
+	})
+	expected2 := user{
+		name: "Olivia",
+		age:  42,
+	}
+	a.Equal(expected2, result2)
+}
+
+func BenchmarkFindByIntegerSlice(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		input := []int64{-100, -5, 30, 100}
+		gotil.FindBy(input, func(val int64) bool {
+			return val == 30
+		})
+	}
+}
+
+func BenchmarkFindByStructSlice(b *testing.B) {
+	input2 := []user{
+		{
+			name: "Micheal",
+			age:  27,
+		},
+		{
+			name: "Joe",
+			age:  30,
+		},
+		{
+			name: "Olivia",
+			age:  42,
+		},
+	}
+	for n := 0; n < b.N; n++ {
+		gotil.FindBy(input2, func(val user) bool {
+			return val.name == "Olivia"
+		})
+	}
+}
+
+func ExampleFindBy() {
+	data := []int64{-100, -5, 30, 100}
+	// Input: [-100 -5 30 100]
+	newData := gotil.FindBy(data, func(val int64) bool {
+		return val == 30
+	})
+	fmt.Println(newData)
+	// Output: 30
+}
+
+func TestFindLastBy(t *testing.T) {
+	a := assert.New(t)
+	input := []int64{10, 15, 90, 100, 30, 50}
+	expected := int64(100)
+	result := gotil.FindLastBy(input, func(val int64) bool {
+		return val > 80
+	})
+	a.Equal(expected, result)
+	input2 := []user{
+		{
+			name: "Micheal",
+			age:  27,
+		},
+		{
+			name: "Joe",
+			age:  30,
+		},
+		{
+			name: "Olivia",
+			age:  42,
+		},
+		{
+			name: "Mike",
+			age:  43,
+		},
+	}
+	result2 := gotil.FindBy(input2, func(val user) bool {
+		return val.age == 42
+	})
+	expected2 := user{
+		name: "Olivia",
+		age:  42,
+	}
+	a.Equal(expected2, result2)
+}
+
+func BenchmarkFindLastByIntegerSlice(b *testing.B) {
+	input := []int64{10, 15, 90, 100, 30, 50}
+	for n := 0; n < b.N; n++ {
+		_ = gotil.FindLastBy(input, func(val int64) bool {
+			return val > 80
+		})
+	}
+}
+
+func BenchmarkFindLastByStructSlice(b *testing.B) {
+	input := []user{
+		{
+			name: "Micheal",
+			age:  27,
+		},
+		{
+			name: "Joe",
+			age:  30,
+		},
+		{
+			name: "Olivia",
+			age:  42,
+		},
+		{
+			name: "Mike",
+			age:  43,
+		},
+	}
+	for n := 0; n < b.N; n++ {
+		_ = gotil.FindLastBy(input, func(val user) bool {
+			return val.age == 42
+		})
+	}
+}
+
+func ExampleFindLastBy() {
+	data := []int64{-100, -5, 30, 100}
+	// Input: [-100 -5 30 100]
+	newData := gotil.FindLastBy(data, func(val int64) bool {
+		return val == 30
+	})
+	fmt.Println(newData)
+	// Output: 30
+}
