d359ed09a925c9eed6e8eca6a1516cf43e018498
hayrullahcansu <hayrullah.cansu@gmail.com>
2022-05-02 21:24:16 +0300

    feature: impl group by & test generic support

--- group_by.go
+++ group_by.go
@@ -1,17 +1,18 @@
 package gotil
 
-import (
-	"github.com/gotilty/gotil/internal"
-)
-
-// GroupBy iterates over elements of collection, returns an object of fist element predicate if returns true for.
-//	data := []float64{5, 10.5, 10, 20, 10.75, 100, 4.23, 5.15, 5.99, 100.0001}
-// 	if result, err := GroupBy(data, func(a interface{}, i interface{}) interface{} {
-// 		return math.Floor(a.(float64))
-// 	}); err == nil {
-// 		fmt.Println(result)
-// 	}
-// 	// Output: map[4:[4.23] 5:[5 5.15 5.99] 10:[10.5 10 10.75] 20:[20] 100:[100 100.0001]]
-func GroupBy(a interface{}, f func(val, i interface{}) interface{}) (interface{}, error) {
-	return internal.GroupBy(a, f)
+// GroupBy iterates over elements of collection, returns an map object.
+// key is result of iterator method
+// value is list
+// 	result := gotil.GroupBy([]int{10, 20, 30}, func(v int, i int) int {
+// 	return v / 10
+// 	})
+// 	fmt.Println(result)
+// 	// Output: map[1:[10] 2:[20] 3:[30]]
+func GroupBy[T, K comparable](s []T, f func(v T, i int) K) map[K][]T {
+	result := map[K][]T{}
+	for i, v := range s {
+		key := f(v, i)
+		result[key] = append(result[key], v)
+	}
+	return result
 }
