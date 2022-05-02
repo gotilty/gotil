edb32b62320cd424efb1493b9e7c0f79040efb4b
hayrullahcansu <hayrullah.cansu@gmail.com>
2022-05-02 21:44:11 +0300

    feature: filter & test - support generic

--- filter.go
+++ filter.go
@@ -1,16 +1,17 @@
 package gotil
 
-import "github.com/gotilty/gotil/internal"
-
 // FilterBy iterates over elements of collection, returning an array of all elements predicate if returns true for.
-//	newData, _ := FilterBy([]int64{-100, -5, 30, 100}, func(val interface{}, i int) bool {
-//	if val.(int64) > 0 {
-// 		return true
-// 		} else {
-// 			return false
-// 		}
+// 	result := gotil.FilterBy([]int64{-100, -5, 30, 100}, func(v int64, i int) bool {
+// 	return v > 0
 // 	})
+// 	fmt.Println(result)
 // 	// Output: [30 100]
-func FilterBy(a interface{}, f func(val interface{}, i int) bool) (interface{}, error) {
-	return internal.FilterBy(a, f)
+func FilterBy[T any](s []T, f func(v T, i int) bool) []T {
+	result := []T{}
+	for i, v := range s {
+		if f(v, i) {
+			result = append(result, v)
+		}
+	}
+	return result
 }
